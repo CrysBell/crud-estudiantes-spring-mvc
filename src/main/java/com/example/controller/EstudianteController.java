14335a2fa7c3c46c2ba4c391e3f871abfd1a086c
Crysbell <erikacrystal95@gmai.com>
2026-06-19 13:33:52 +0200

    desarrollo estructura formulario alta y  añadir metodo para dar de alta a estudiantes

--- src/main/java/com/example/controller/EstudianteController.java
+++ src/main/java/com/example/controller/EstudianteController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entities.Estudiante;
 import com.example.services.EstudianteService;
+import com.example.services.FacultadService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EstudianteController {
     
     private final EstudianteService estudianteService;
+    private final FacultadService facultadService;
 
     
     //@GetMapping("/listar")
@@ -33,4 +36,18 @@ public class EstudianteController {
         //Mostrar la vista del archvo HTML
         return "listadoEstudiantes";
     }
+
+     //Metodo que muestra el formulario de creación de estudiantes
+    @GetMapping("/alta")
+    public String mostrarFormularioAlta(Model model){
+    //Se necesitan los facultads desde la capa de servicios
+        model.addAttribute("facultades", facultadService.getAllFacultades());
+
+    //Se necesita enviar un objeto Estudiante vacio, para que se vinculen sus propiedades
+    //con cada control (element, input, select, etc) del atributo
+        model.addAttribute("estudiantes", new Estudiante());
+
+        return "formularioAltaModificacion";
+    }
+
 }
